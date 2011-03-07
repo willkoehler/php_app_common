d7ebad3c3b0ca7173d47270622e33622b9fa4625
Will Koehler <wck555@gmail.com>
2011-03-06 23:54:47 -0500

    Added GeneratePassword()

--- RequestHelpers.php
+++ RequestHelpers.php
@@ -276,6 +276,45 @@ function MakePasswordHash($pw, $salt = null)
 
 
 //----------------------------------------------------------------------------------
+//  GeneratePassword()
+//
+//  Generates a random password string.
+//
+//  PARAMETERS:
+//    length  - length of the password
+//    level   - level of password complexity
+//
+//  RETURN: generated password
+//-----------------------------------------------------------------------------------
+function GeneratePassword($length=6, $level=2)
+{
+    list($usec, $sec) = explode(' ', microtime());
+    srand((float) $sec + ((float) $usec * 100000));
+
+    $validchars[1] = "0123456789abcdfghjkmnpqrstvwxyz";
+    $validchars[2] = "0123456789abcdfghjkmnpqrstvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $validchars[3] = "0123456789_!@#$%&*()-=+/abcdfghjkmnpqrstvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_!@#$%&*()-=+/";
+
+    $password  = "";
+    $counter   = 0;
+
+    while ($counter < $length)
+    {
+        $actChar = substr($validchars[$level], rand(0, strlen($validchars[$level])-1), 1);
+
+        // All character must be different
+        if (!strstr($password, $actChar))
+        {
+            $password .= $actChar;
+            $counter++;
+        }
+    }
+
+    return $password;
+}
+
+
+//----------------------------------------------------------------------------------
 //   GetBrowserString()
 //
 //   This function builds a string describing the client's browser.

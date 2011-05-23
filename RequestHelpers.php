4a1eeb5004dd75c9b476c4256e41fec397ce4367
Will Koehler <wck555@gmail.com>
2011-05-22 23:16:49 -0400

    Added ParseURLs() function.

--- RequestHelpers.php
+++ RequestHelpers.php
@@ -468,7 +468,42 @@ function ConvertToHTML($strText)
         $strConverted .= "</ul>";
     } 
     return $strConverted;
-} 
+}
+
+
+//----------------------------------------------------------------------------------
+//   ParseURLs()
+//
+//   This function searches the give text for URLS and adds HTML tags to make the
+//   URLs clickable.
+//
+//  PARAMETERS:
+//     text   - text to search for URLs
+//
+//  RETURN: text with HTML tags added to make them clickable
+//-----------------------------------------------------------------------------------
+function ParseURLs($text, $maxurl_len = 35, $target = '_blank')
+{
+    if (preg_match_all('/((ht|f)tps?:\/\/([\w\.]+\.)?[\w-]+(\.[a-zA-Z]{2,4})?[^\s\r\n\(\)"\'<>\,\!]+)/si', $text, $urls))
+    {
+        $offset1 = ceil(0.65 * $maxurl_len) - 2;
+        $offset2 = ceil(0.30 * $maxurl_len) - 1;
+
+        foreach (array_unique($urls[1]) AS $url)
+        {
+            if ($maxurl_len AND strlen($url) > $maxurl_len)
+            {
+                $urltext = substr($url, 0, $offset1) . '…' . substr($url, -$offset2);
+            }
+            else
+            {
+                $urltext = $url;
+            }
+            $text = str_replace($url, '<a href="'. $url .'" target="'. $target .'" title="'. $url .'">'. $urltext .'</a>', $text);
+        }
+    }
+    return $text;
+}
 
 
 //----------------------------------------------------------------------------------

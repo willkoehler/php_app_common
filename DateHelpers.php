b825eeb0bba9f49b911dd4142ef5cdb5695d1d1f
Will Koehler <wck555@gmail.com>
2012-04-11 13:55:56 -0400

    iGetQuarterFromMonth() now returns zero-based quarter.

--- DateHelpers.php
+++ DateHelpers.php
@@ -35,7 +35,7 @@ function dtNumericToDate($iNumeric)
 //----------------------------------------------------------------------------------
 //   iGetQuarterFromMonth()
 //
-//   This function returns the quarter (one-based) that a given month belongs to
+//   This function returns the quarter (zero-based) that a given month belongs to
 //
 //  PARAMETERS:
 //    Int iMonth    - Month
@@ -49,22 +49,22 @@ function iGetQuarterFromMonth($iMonth)
         case 1:
         case 2:
         case 3:
-            return(1);
+            return(0);
             break;
         case 4:
         case 5:
         case 6:
-            return(2);
+            return(1);
             break;
         case 7:
         case 8:
         case 9:
-            return(3);
+            return(2);
             break;
         case 10:
         case 11:
         case 12:
-            return(4);
+            return(3);
             break;
     } 
 } 
